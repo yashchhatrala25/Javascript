9d62cdd7f15f8ecd22003199a75059c4178c9ea9
yashchhatrala25 <159877576+yashchhatrala25@users.noreply.github.com>
2024-04-23 16:27:12 +0000

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3); // we are using nodejs, not browser
+
+console.log(3
+    +
+    3); // code readability should be high 
+console.log("Yash");
+
+let name = "Yash";
+let age = 25;
+let isLoggedIn = true;
+let state;
+
+// number => 2 to power of 53
+// bigInt 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
