b8215da0347cf41e3845da2236376f47f707740e
yashchhatrala25 <159877576+yashchhatrala25@users.noreply.github.com>
2024-04-25 16:42:06 +0000

    ConversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+// let score = "33abc";
+// let score = null;
+// let score = undefined;
+// let score = true;
+let score = false;
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN 
+// true => 1; false => 0;
+
+// let isLoggedIn = 1;
+// let isLoggedIn = "";
+let isLoggedIn = "yash";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true;
+// 0 => false;
+// "" => false;
+// "yash" => true;
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
