b6c57e39324b0e86de88edab92938208bee143d9
Yash Chhatrala <159877576+yashchhatrala25@users.noreply.github.com>
2024-04-26 18:53:43 +0000

    comparision operators

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// Comparision and equality both are different in js
+
+// console.log(null > 0); // false
+// console.log(null == 0); // false
+// console.log(null >= 0); // true
+
+// console.log(undefined > 0); // false
+// console.log(undefined == 0); // false
+// console.log(undefined >= 0); // false
+
+// === check value strictly
+console.log("2" === 2); // false
+console.log(2 === 2); // true
