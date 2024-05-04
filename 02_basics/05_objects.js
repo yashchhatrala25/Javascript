a92dc20ca91651df1be72c59c31a4c863f467447
yashchhatrala25 <yashchhatrala25@gmail.com>
2024-05-04 12:43:01 +0530

    object 3 and json

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,20 @@
+const course = {
+    courseName: "Js in Hindi",
+    price: "999",
+    courseInstructor: "Hitesh"
+}
+
+// course.courseName
+const {courseName, price, courseInstructor: instructor} = course;
+// console.log(courseName, instructor);
+
+// const navbar = ({company}) => {
+
+// }
+// navbar(company = "Yash")
+
+// {
+//     "name": "yash",
+//     coursename: "js in hindi",
+//     "price": "free"
+// }
