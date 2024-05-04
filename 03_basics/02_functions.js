2b7421ce16e1c2b9e2312f495876e0cb76794ae6
yashchhatrala25 <yashchhatrala25@gmail.com>
2024-05-04 23:18:01 +0530

    function 2

---
+++ 03_basics/02_functions.js
@@ -0,0 +1,26 @@
+function calculateCartPrice(val1, val2, ...num1) {
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 300, 400, 2000));
+
+// const user = {
+//     username: "Yash",
+//     prices: 199
+// }
+
+function handleObject(anyobject) {
+    console.log(`Username is ${anyobject.username} price is ${anyobject.price}`);
+}
+// handleObject(user);
+handleObject({
+    username: "sam",
+    price: 199
+});
+
+const myNewArray = [200, 400, 100, 600];
+function returnSecondValue(getArray) {
+    return getArray[2]
+}
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 100, 600]));
