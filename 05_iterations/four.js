400f82c333d54196e2678c52229ee098ca4cb45a
yashchhatrala25 <yashchhatrala25@gmail.com>
2024-05-07 09:07:42 +0530

    ojects, array, forof

---
+++ 05_iterations/four.js
@@ -0,0 +1,34 @@
+const myObj = {
+  js: 'JavaScript',
+  cpp: 'C++',
+  rb: 'Ruby',
+  py: 'Python'
+}
+
+// for (const key in myObj) {
+//   console.log(key);
+// }
+
+// for (const key in myObj) {
+//   console.log(myObj[key]);
+// }
+
+for (const key in myObj) {
+  // console.log(`${key} shortcut is for ${myObj[key]}`);
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"];
+for (const key in programming) {
+  // console.log(programming[key]);
+}
+
+// const map = new Map();
+// map.set('IN', 'India');
+// map.set('USA', 'United State of America');
+// map.set('FR', 'France');
+// map.set('IN', 'India');
+
+// for (const key in map) {
+//   console.log(key);
+// }
+
