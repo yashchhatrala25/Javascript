5171c9592243ae16a7aca7a702b3efc341ae4ffe
yashchhatrala25 <yashchhatrala25@gmail.com>
2024-05-07 09:05:21 +0530

    while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,24 @@
+let index = 0;
+while (index <= 10) {;
+  // console.log(`Value of index is ${index}`);
+  index = index + 2
+}
+
+const myArray = ["banana", "mango", "orange"];
+let arr = 0;
+while (arr < myArray.length) {
+  // console.log(`Value is ${myArray[arr]}`);
+  arr++;
+}
+
+let score = 0;
+do {
+  // console.log(`Score is ${score}`);
+  score++;
+} while (score <= 10);
+
+let scoreOne = 11;
+do {
+  console.log(`Score is ${scoreOne}`);
+  score++;
+} while (score <= 10);
